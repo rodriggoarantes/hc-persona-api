194918b1eb0687fdaaa00b7b603b4ad2a4c88890
rodriggoarantes <roevangelist@uolinc.com>
2022-04-01 00:04:30 -0300

    ajusts

--- persona-domain/build.gradle.kts
+++ persona-domain/build.gradle.kts
@@ -7,6 +7,5 @@ plugins {
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
     testImplementation("org.mockito:mockito-core:4.3.1")
-    // testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:${property("mockitoKotlinVersion")}")
 }
 
